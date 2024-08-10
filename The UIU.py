17f798723e536356338c72a71f6b7ef097bb3eef
amudhafar11 <alihayder11@icloud.com>
2024-08-10 17:06:15 -0500

    Initial commit

---
+++ The UIU.py
@@ -0,0 +1,180 @@
+import time
+
+print("-----------------------------------------------------------------")
+print("Welcome to the Universal Internet Unit (UIU)")
+print("You have been Hired. Answer incoming requests and calls. Good luck on the job! The Internet is in YOUR hands!")
+print("-----------------------------------------------------------------")
+print("Waiting for Requests and calls...")
+time.sleep(1)
+print("-----------------------------------------------------------------")
+print("INCOMING REQUEST")
+print("TYPE 'READ' TO READ(without apostrophe)")
+answer = 0
+
+while answer != "READ":
+
+    answer = input()
+    if answer == 'READ':
+        print("Loading...")
+        time.sleep(1)
+    else:
+        print("JOB FAILED. RESTART TO TRY AGAIN")
+print("-----------------------------------------------------------------")
+print("Hello UIU!!! I am a mother of 3 kids, and they always are on that damn phone thingyyy! Can you turn off the "
+      "internet for just a tinnnny bit?")
+print("-----------------------------------------------------------------")
+print("TYPE 'SURE' IF YOU ACCEPT, 'NO' IF YOU DECLINE")
+answer = 0
+
+while answer != "SURE":
+
+    answer = input()
+    if answer == 'SURE':
+        print("Loading...")
+        time.sleep(1)
+        print("Great! Thanks for working with you guys!!!")
+    else:
+        print ("JOB FAILED. RESTART TO TRY AGAIN")
+print("-----------------------------------------------------------------")
+print("finding code...")
+time.sleep(1)
+print("TYPE '734623' TO ACCESS UIU INTERNET MANAGEMENT")
+answer = 0
+
+while answer != "734623":
+
+    answer = input()
+    if answer == '734623':
+        print("Loading...")
+        time.sleep(1)
+        print("Welcome to the UIU Management")
+    else:
+        print ("JOB FAILED. RESTART TO TRY AGAIN")
+print("TYPE 'I CONSENT' TO TURN OFF INTERNET")
+answer = 0
+
+while answer != "I CONSENT":
+
+    answer = input()
+    if answer == 'I CONSENT':
+        print("Loading")
+        time.sleep(1)
+        print("PLEASE SELECT TIME")
+    else:
+        print("JOB FAILED. RESTART TO TRY AGAIN")
+print("1 SECOND(TOO SHORT)|2 SECONDS(TOO SHORT)|3 SECONDS")
+answer = 0
+
+while answer != "3 SECONDS":
+
+    answer = input()
+    if answer == '3 SECONDS':
+        print("Shutting down internet...")
+        time.sleep(1)
+        print("Internet Shutdown complete.")
+        print("turning on in")
+        print("3")
+        time.sleep(1)
+        print("2")
+        time.sleep(1)
+        print("1")
+        time.sleep(1)
+        print("Turning on...")
+    else:
+        print ("JOB FAILED. RESTART TO TRY AGAIN")
+time.sleep(1)
+print("   ")
+print("INTERNET TURN-ON FAILED")
+print("TYPE THE GIVEN CODE TO ACCESS EMERGENCY SUPPORT CATALOG")
+print("Finding Code...")
+time.sleep(1)
+print("653434")
+answer = 0
+
+while answer != "653434":
+
+    answer = input()
+    if answer == '653434':
+        print("Loading...")
+        time.sleep(1)
+    else:
+        print ("JOB FAILED. RESTART TO TRY AGAIN")
+print("TYPE 'INTERNET RESET' TO READ INTERNET RESET ARTICLE. TYPE 'HOW TO GET MCDONALDS BURGER' TO READ HOW TO GET "
+      "MCDONALDS BURGER ARTILE")
+answer = 0
+
+while answer != "INTERNET RESET":
+
+    answer = input()
+    if answer == 'INTERNET RESET':
+        print("To reset the universal internet, type the code '878787. If this fails, email the Head Emergency Unit ("
+              "HEU) via the code '232345'. This information may be needed in the future.")
+    else:
+        print("JOB FAILED. RESTART TO TRY AGAIN")
+answer = 0
+
+while answer != "878787":
+
+    answer = input()
+    if answer == '878787':
+        print("Resetting universal internet...")
+        time.sleep(1)
+        print("Reset Failed")
+    else:
+        print ("JOB FAILED. RESTART TO TRY AGAIN")
+print("loading...")
+time.sleep(1)
+print("Would you like to email help? 'YES' or 'NO'")
+answer = 0
+
+while answer != "YES":
+
+    answer = input()
+    if answer == 'YES':
+        print("Please enter the email code:")
+    else:
+        print ("JOB FAILED. RESTART TO TRY AGAIN")
+answer = 0
+
+while answer != "232345":
+
+    answer = input()
+    if answer == '232345':
+        print("Waiting for response...")
+        time.sleep(1)
+        print("HEU: Hello? This is HEU.")
+        time.sleep(1)
+        print("YOU: Hello. I need help with turning on the universal internet.")
+        time.sleep(1)
+        print("HEU: I see the problem..")
+        time.sleep(1)
+        print("HEU: Use the code 555555 to reset the main system, that should work. Oh yeah, and to access the Reset "
+              "Center, use this code: 123456.")
+
+    else:
+        print ("JOB FAILED. RESTART TO TRY AGAIN")
+answer = 0
+while answer != "123456":
+    answer = input()
+    if answer == '123456':
+        print("Entering Reset Center...")
+        time.sleep(1)
+        print("Welcome to the Reset Center.")
+    else:
+        print("JOB FAILED. RESTART TO TRY AGAIN")
+print("Please enter reset code: ")
+answer = 0
+
+while answer != "555555":
+
+    answer = input()
+    if answer == '555555':
+        print("Resetting...")
+        time.sleep(1)
+        print("Successfully Restarted. Thank you for your service to the UIU. ")
+    else:
+        print ("JOB FAILED. RESTART TO TRY AGAIN")
+print("-----------------------------------------------------------------")
+
+
+
